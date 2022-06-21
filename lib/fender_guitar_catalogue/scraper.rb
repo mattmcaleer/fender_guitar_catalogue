d5ec026692eb12392f0efcbbf195119ae3231fd2
Matthew <matthew_mcaleer@hotmail.com>
2022-06-21 15:11:49 +1000

    refactored scraper class"

--- lib/fender_guitar_catalogue/scraper.rb
+++ lib/fender_guitar_catalogue/scraper.rb
@@ -1,54 +1,40 @@
 class Scraper
+
+    GUITAR_TYPE = { 
+        "bass" => "electric-basses/?start=0&sz=200",
+        "electric" => "electric-guitars/?start=0&sz=200",
+        "acoustic" => "acoustic-guitars/?start=0&sz=200"
+    }
     
-BASE_URL = "https://www.fender.com/en-AU/"
+    BASE_URL = "https://www.fender.com/en-AU/"
 
-def get_page(url = BASE_URL)
-    Nokogiri::HTML(open(url))
-end
-
-def scrape_test
-    doc = get_page(url = "#{BASE_URL}electric-guitars/?start=0&sz=200")
-
-    doc.css(".product-tile").each.with_index(1).map do |guitar, i|
-        model = guitar.css(".pdp-link").text.strip
-        price = guitar.css(".price").text.strip
-        url = guitar.css("a.product-tile-image").attr("href").value
-
-        puts "#{i}. #{model}"
-        Guitar.new(model: model, price: price, url: url)
-    end
-end
-
-def scrape(type)
-
-    if type == "bass"
-        doc = get_page(url = "#{BASE_URL}electric-basses/?start=0&sz=200")
-    elsif type == "electric"
-        doc = get_page(url = "#{BASE_URL}electric-guitars/?start=0&sz=200")
-    elsif type == "acoustic"
-        doc = get_page(url = "#{BASE_URL}acoustic-guitars/?start=0&sz=200")
+    def get_page(url = BASE_URL)
+        Nokogiri::HTML(open(url))
     end
 
-    doc.css(".product-tile").each.with_index(1).map do |guitar, i|
-        model = guitar.css(".pdp-link").text.strip
-        price = guitar.css(".price").text.strip
-        url = guitar.css("a.product-tile-image").attr("href").value
+    def scrape(type)
+        doc = get_page(url = "#{BASE_URL}#{GUITAR_TYPE[type]}")
+    
+        doc.css(".product-tile").each.with_index(1).map do |guitar, i|
+            model = guitar.css(".pdp-link").text.strip
+            price = guitar.css(".price").text.strip
+            url = guitar.css("a.product-tile-image").attr("href").value
 
-        puts "#{i}. #{model}"
-        Guitar.new(model: model, price: price, url: url)
+            puts "#{i}. #{model}"
+            Guitar.new(model: model, price: price, url: url)
+        end
     end
-end
 
-def scrape_content(url)
-    page_url = "#{BASE_URL}#{url}"
-    doc = get_page(page_url)
+    def scrape_content(url)
+        page_url = "#{BASE_URL}#{url}"
+        doc = get_page(page_url)
 
-    puts "#{doc.css(".row.long-description p").text.strip.gsub("Click here to learn more about this artist model.", "")}"
-    puts ""
-    puts "~Features~"
-    puts ""
-    li = "#{doc.css(".js-feature-bullets li")}"
-    puts li.gsub("<li>","* ").gsub("</li>","")
-end
+        puts "#{doc.css(".row.long-description p").text.strip.gsub("Click here to learn more about this artist model.", "")}"
+        puts ""
+        puts "~Features~"
+        puts ""
+        li = "#{doc.css(".js-feature-bullets li")}"
+        puts li.gsub("<li>","* ").gsub("</li>","")
+    end
 
 end
